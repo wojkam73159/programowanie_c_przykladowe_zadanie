65cf61bb440490a45832261b7827f085a45eb634
wojtek kaminski <wojtek.kamlot@gmail.com>
2019-05-22 01:02:18 +0200

    przykladowy program

--- komputery_baza/komputery_baza.cpp
+++ komputery_baza/komputery_baza.cpp
@@ -2,20 +2,53 @@
 //
 
 #include "pch.h"
-#include <iostream>
+#include <cstdlib>
+#include<fstream>
+
+bool wa(unsigned int liczba)//5 jedynek binarnych 
+{
+	int jedynki = 0;
+	for (int i = 0; i < 32; i++)
+	{
+		if ( liczba%2)
+		{
+			jedynki++;
+		}
+		liczba >>= 1;
+	}
+
+	if (jedynki == 5)
+		return true;
+	else return false;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	FILE * plikin, *alfa ,*beta;
+	  fopen_s(&plikin,"odczyty.txt","r");
+	 fopen_s(&alfa,"alfa.txt", "w");
+	  fopen_s(&beta,"beta.txt", "w");
+	unsigned int temp;
+	unsigned int porzadkowa_liczba = 0;
+	while (fscanf_s(plikin,"%u",&temp)!=EOF)
+	{
+		if (wa(temp))
+		{
+			porzadkowa_liczba++;
+			continue;
+		}
+
+		if (porzadkowa_liczba % 2)
+		{
+			fprintf(beta, "%u", temp);
+		}
+		else
+		{
+			fprintf(alfa, "%u", temp);
+		}
+
+		
+	}
+	return 0;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
